bc4c79462b57f1aade1017b7e4d7f0e8840a7c00
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-02-25 10:28:53 +0530

    commit1
    animation synced

---
+++ Assets/Scripts/CowBoyManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CowBoyManager : MonoBehaviourPun
+{
+    public GameObject cam;
+    public float moveSpeed=5;
+    public SpriteRenderer sprite;
+    public Animator anim;
+    public PhotonView photonView;
+    private bool allowMovement= true;
+    // Start is called before the first frame update
+    void Awake()
+    {
+       if(photonView.IsMine)
+        {
+            cam.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(photonView.IsMine)
+        {
+            checkMoments();
+        }
+
+
+        
+    }
+
+    private void checkMoments()
+    {
+        if(allowMovement)
+        {
+            var Movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+            transform.position += Movement * moveSpeed * Time.deltaTime;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.RightControl) && anim.GetBool("Ismove")==false)
+        {
+            shoot();
+        }else if(Input.GetKeyUp(KeyCode.RightControl))
+        {
+            anim.SetBool("IsSHot", false);
+            allowMovement = true;
+        }
+        if(Input.GetKeyDown(KeyCode.D) && anim.GetBool("IsSHot")==false)
+        {
+            anim.SetBool("Ismove", true);
+            //sprite.flipX = false; not synched over network
+            photonView.RPC("FlipRight", RpcTarget.AllBuffered);
+        }
+       else if(Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetBool("Ismove", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A) && anim.GetBool("IsSHot") == false)
+        {
+            anim.SetBool("Ismove", true);
+            //sprite.flipX = true;
+            photonView.RPC("FlipLeft", RpcTarget.AllBuffered);
+        }
+       else if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetBool("Ismove", false);
+        }
+    }
+
+    private void shoot()
+    {
+        anim.SetBool("IsSHot", true);
+        allowMovement = false;
+    }
+    [PunRPC]
+    private void FlipRight()
+    {
+        sprite.flipX = false;
+    }
+    [PunRPC]
+    private void FlipLeft()
+    {
+        sprite.flipX = true;
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+public class MenuManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject userNameScreen, connectScreen;
+    [SerializeField]
+    private GameObject usernameButton;
+    [SerializeField]
+    private InputField username_Input, room_Create_Input, room_join_Input;
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server!");
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Connected to Lobby!");
+        userNameScreen.SetActive(true);
+    }
+
+    #region UICALLS
+    public void OnClick_usernameButton()
+    {
+        PhotonNetwork.NickName = username_Input.text;
+        userNameScreen.SetActive(false);
+        connectScreen.SetActive(true);
+    }
+
+    public void OnNameField_Changed()
+    {
+        if (username_Input.text.Length >= 2)
+        {
+            usernameButton.SetActive(true);
+        }
+        else
+        {
+            usernameButton.SetActive(false);
+        }
+
+    }
+    public override void OnJoinedRoom()
+    {
+        //start the game
+
+        PhotonNetwork.LoadLevel(1);
+
+    }
+    #endregion
+    public void OnClick_JoinRoom()
+    {
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(room_join_Input.text, ro, TypedLobby.Default);
+    }
+    public void OnClick_CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(room_Create_Input.text, new RoomOptions { MaxPlayers = 4 }, null);
+    }
+
+}
